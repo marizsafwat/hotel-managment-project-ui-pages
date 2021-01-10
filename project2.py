18ded91b00817b396e841ff915140c29be19b2f6
marizsafwat <zozasokar@hotmail.com>
2021-01-10 17:18:42 +0200

    mm

---
+++ project2.py
@@ -0,0 +1,33 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from PyQt5.uic import loadUiType
+import sys
+
+from PyQt5.uic.properties import QtWidgets
+
+MainUI,_ = loadUiType('intro2.ui')
+class Main(QMainWindow, MainUI):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.setWindowTitle("Pure life hotels")
+
+
+
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__' :
+    main()
